9bc1c94857ced24de814924eeac4d6d8aa3899ec
MathWave <emmtvv@icloud.com>
2018-05-22 14:56:42 +0300

    v2.0
    - Добавлен графический интерфейс

--- main.py
+++ main.py
@@ -1,5 +1,33 @@
+from tkinter import *
 from random import randrange
 
+root = Tk()
+root.title("RSA by MathWave")
+root.geometry('300x300')
+root.resizable(width=False, height=False)
+
+label_private = Label(root, text='Закрытый ключ: ')
+label_private.place(x=10, y=10)
+label_public = Label(root, text='Открытый ключ: ')
+label_public.place(x=10, y=40)
+
+canvas_private = Canvas(root, width=120, height=20, bg='black')
+canvas_private.place(x=120, y=10)
+canvas_public = Canvas(root, width=120, height=20, bg='black')
+canvas_public.place(x=120, y=40)
+
+label_key = Label(root, text='Ключ:')
+label_key.place(x=10, y=100)
+label_text = Label(root, text='Сообщение:')
+label_text.place(x=10, y=130)
+
+entry_key = Entry(root)
+entry_text = Entry(root)
+entry_key.place(x=100, y=100)
+entry_text.place(x=100, y=130)
+
+canvas_text = Canvas(root, width=290, height=130, bg='black')
+canvas_text.pack(side='bottom')
 
 def IsPrime(n):
     i = 2
@@ -9,7 +37,6 @@ def IsPrime(n):
         i += 1
     return True
 
-
 def NOD(a, b):
     while a != 0 and b != 0:
         if a > b:
@@ -18,38 +45,15 @@ def NOD(a, b):
             b = b % a
     return max(a, b)
 
-
 def FindExp(phi):
     return e
-
 
 def FindSecretExp(phi, e):
     for d in range(phi):
         if (d * e) % phi == 1:
             return d
 
-command = input("Enter 's' to send the message, 'r' to recieve or 'k' to generate the key: ")
-if command == 's':
-    coms = input("Enter the public key: ").split()
-    message = input("Enter the message: ")
-    ord_letters = []
-    for letter in message:
-        ord_letters.append(ord(letter))
-    new_text = ""
-    for number in ord_letters:
-        new_text += (str((number ** int(coms[0])) % int(coms[1])) + " ")
-    print("Encrypted message: " + new_text)
-elif command == 'r':
-    coms = input("Enter your private key: ").split()
-    message = input("Enter the message: ").split()
-    int_message = []
-    for i in message:
-        int_message.append(int(i))
-    new_text = ""
-    for number in int_message:
-        new_text += chr((number ** int(coms[0])) % int(coms[1]))
-    print("Decrypted message: " + new_text)
-elif command == 'k':
+def CreateKey():
     p = 10
     q = 10
     while not IsPrime(p):
@@ -62,7 +66,45 @@ elif command == 'k':
     while NOD(e, phi) != 1:
         e = (e + 1)
     d = FindSecretExp(phi, e)
-    print("Here is your SECRET key, do not tell it to anyone: " + str(d) + " " + str(n))
-    print("Here is your PUBLIC key, send it to your friend: " + str(e) + " " + str(n))
-else:
-    print("Command not found!")
+    canvas_private.delete('all')
+    canvas_public.delete('all')
+    canvas_private.create_text(10, 12, anchor=W, fill='white', text=(str(d) + " " + str(n)))
+    canvas_public.create_text(10, 12, anchor=W, fill='white', text=(str(e) + " " + str(n)))
+
+def Encrypt():
+    coms = entry_key.get().split()
+    message = entry_text.get()
+    ord_letters = []
+    for letter in message:
+        ord_letters.append(ord(letter))
+    new_text = ""
+    for number in ord_letters:
+        new_text += (str((number ** int(coms[0])) % int(coms[1])) + " ")
+    canvas_text.delete('all')
+    canvas_text.create_text(145, 65, width=250, fill='white', text=new_text)
+
+def Decrypt():
+    coms = entry_key.get().split()
+    message = entry_text.get().split()
+    int_message = []
+    for i in message:
+        int_message.append(int(i))
+    new_text = ""
+    for number in int_message:
+        new_text += chr((number ** int(coms[0])) % int(coms[1]))
+    canvas_text.delete('all')
+    canvas_text.create_text(145, 65, width=250, fill='white', text=new_text)
+
+btn_generate = Button(root, text="Сгенерировать!")
+btn_generate.bind('<Button-1>', lambda event: CreateKey())
+btn_generate.place(x=10, y=70)
+
+btn_crypt = Button(root, text="->")
+btn_crypt.bind('<Button-1>', lambda event: Encrypt())
+btn_crypt.place(x=150, y=70)
+
+btn_crypt = Button(root, text="<-")
+btn_crypt.bind('<Button-1>', lambda event: Decrypt())
+btn_crypt.place(x=200, y=70)
+
+root.mainloop()
